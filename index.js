22b5ba799ab0d10073b6ef15fd24170291057a60
Julius Kedienye <juliuskedienye61@gmail.com>
2025-06-28 17:14:33 +0300

    my first commit

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 function addingEventListener() {
+  const input = document.getElementById('button');
+  input.addEventListener('click', function () {
+    alert('I was clicked!');
+  });
+}
+
+// Export if running in Node environment (e.g., for testing)
+if (typeof module !== 'undefined' && module.exports) {
+  module.exports = { addingEventListener };
 }
